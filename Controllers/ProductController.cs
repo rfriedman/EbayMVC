678084df212fd43eec89bc61d449bd4ef9ce8659
richard friedman <friedman.richard@gmail.com>
2012-09-16 22:58:15 -0400

    initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EbayMVC.Models;
+using EbayMVC.ServiceReference2;
+
+namespace EbayMVC.Controllers
+{ 
+    public class ProductController : Controller
+    {
+        private ProductsDBContext db = new ProductsDBContext();
+
+        //
+        // GET: /Product/
+
+        public ViewResult Index()
+        {
+            
+            List<Products> prodList = new List<Products> { };
+            EbayClientClient c = new EbayClientClient();
+
+            var result = c.ItemByCategory("48818").ToList();
+            foreach (tblItem t in result)
+            {
+                Products p = new Products
+                {
+                    Id = t.item_id,
+                    item_title = t.item_title,
+                    gallery_url = t.gallery_url,
+                    listing_url = t.listing_url,
+                    tblCategory = t.item_category
+                };
+                prodList.Add(p);
+            }
+            
+            return View(prodList);
+        }
+
+        //
+        // GET: /Product/Details/5
+
+        public ViewResult Details(string id)
+        {
+            Products products = db.Store.Find(id);
+            return View(products);
+        }
+
+        //
+        // GET: /Product/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Product/Create
+
+        [HttpPost]
+        public ActionResult Create(Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Store.Add(products);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(products);
+        }
+        
+        //
+        // GET: /Product/Edit/5
+ 
+        public ActionResult Edit(string id)
+        {
+            Products products = db.Store.Find(id);
+            return View(products);
+        }
+
+        //
+        // POST: /Product/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(products).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(products);
+        }
+
+        //
+        // GET: /Product/Delete/5
+ 
+        public ActionResult Delete(string id)
+        {
+            Products products = db.Store.Find(id);
+            return View(products);
+        }
+
+        //
+        // POST: /Product/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string id)
+        {            
+            Products products = db.Store.Find(id);
+            db.Store.Remove(products);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace EbayMVC.Models
+{
+    public class Products
+    {
+        
+        public string Id { get; set; }
+        public string listing_url { get; set; }
+        public string gallery_url { get; set; }
+        public string item_title { get; set; }
+        public string tblCategory { get; set; }
+    }
+
+    public class ProductsDBContext : DbContext
+    {
+        public DbSet<Products> Store { get; set; }
+    }
+}
